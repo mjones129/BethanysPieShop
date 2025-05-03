2b777d163a0a3ad9a42d581915341a9d587cadfb
Matt <me@mattjones.tech>
2025-05-03 11:02:21 -0400

    update database to include shopping cart data

---
+++ BethanysPieShop/Migrations/20250503145818_AddShoppingCartItem.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BethanysPieShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShoppingCartItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShoppingCardId",
+                table: "ShoppingCartItems",
+                newName: "ShoppingCartId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShoppingCartId",
+                table: "ShoppingCartItems",
+                newName: "ShoppingCardId");
+        }
+    }
+}
